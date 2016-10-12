c9f6705ae11e03e2d9cd930fc60ee2803c580a8a
Dean Gardiner <me@dgardiner.net>
2016-10-12 14:03:52 +1300

    Implemented the `FirefoxExtension` `getCallbackPattern()` and `getCallbackUrl()` methods

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,23 @@
 import WebExtensionsExtension from 'eon.extension.browser.base.webextensions/extension';
 
+import URI from 'urijs';
+
 
 export class FirefoxExtension extends WebExtensionsExtension {
+    getCallbackPattern() {
+        let prefix = new URI('https://self')
+            .segmentCoded([this.id])
+            .toString();
+
+        return prefix + '/*';
+    }
+
+    getCallbackUrl(id, path) {
+        return new URI('https://self')
+            .segmentCoded([this.id, id])
+            .segment(path)
+            .toString();
+    }
 }
 
 export default new FirefoxExtension();
