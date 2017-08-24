69752e7c53580fddd5d3e44d77876b36f5729525
Dean Gardiner <me@dgardiner.net>
2017-08-24 14:52:59 +1200

    Added the `FirefoxStorage.browser` property

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,7 +1,14 @@
 import {WebExtensionsStorage} from 'eon.extension.browser.base.webextensions/storage';
 
+import FirefoxExtension from 'eon.extension.browser.firefox/extension';
+
 
 export class FirefoxStorage extends WebExtensionsStorage {
+    get browser() {
+        return {
+            extension: FirefoxExtension
+        };
+    }
 }
 
 export default new FirefoxStorage();
