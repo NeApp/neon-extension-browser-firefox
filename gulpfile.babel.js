0d1ecadbfe6643fd113c486ea8cdb108df0aa4d6
Dean Gardiner <me@dgardiner.net>
2017-08-27 17:05:17 +1200

    Updated [gulpfile.babel.js] to build and archive a development version of the extension

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 import gulp from 'gulp';
 import gutil from 'gulp-util';
 import gzip from 'gulp-zip';
+import rename from 'gulp-rename';
+import {exec} from 'child_process';
 
 import Configuration from './config';
 import Assets from './scripts/build/assets';
@@ -12,8 +14,14 @@ import Webpack from './scripts/build/webpack';
 
 gulp.task('build', [
     'build:clean',
+
+    // Build
+    'build:development',
     'build:production',
-    'build:archive'
+
+    // Archive
+    'build:archive:development',
+    'build:archive:production',
 ]);
 
 gulp.task('build:clean', () => {
@@ -22,22 +30,28 @@ gulp.task('build:clean', () => {
     ]);
 });
 
-gulp.task('build:archive', ['build:development'], () => {
-    // Read extension manifest
-    let manifest = JSON.parse(fs.readFileSync(
-        'build/unpacked/development/manifest.json'
-    ));
-
-    // Create archive of build
-    return gulp.src('build/unpacked/development/**/{*.css,*.html,*.js,*.json}')
-        .pipe(gzip('Eon-' + manifest.version + '-firefox.zip'))
-        .pipe(gulp.dest('build'));
-});
+// region build:production
 
 gulp.task('build:production', ['build:development'], () => {
     // Create production build
-    return gulp.src('build/unpacked/development/**/{*.css,*.html,*.js,*.json}')
+    return gulp.src('build/unpacked/development/**/*')
         .pipe(gulp.dest('build/unpacked/production'));
+});
+
+gulp.task('build:archive:production', ['build:production'], () => {
+    // Read extension manifest
+    let manifest = JSON.parse(fs.readFileSync(
+        'build/unpacked/production/manifest.json'
+    ));
+
+    // Create archive of build
+    return gulp.src('build/unpacked/production/**/*')
+        .pipe(gzip('Neon-' + manifest.version + '-firefox.zip'))
+        .pipe(gulp.dest('build'));
+});
+
+// endregion
+
 });
 
 // region build:development
@@ -47,6 +61,18 @@ gulp.task('build:development', [
     'build:development:manifest',
     'build:development:webpack'
 ]);
+
+gulp.task('build:archive:development', ['build:development'], () => {
+    // Read extension manifest
+    let manifest = JSON.parse(fs.readFileSync(
+        'build/unpacked/development/manifest.json'
+    ));
+
+    // Create archive of build
+    return gulp.src('build/unpacked/development/**/*')
+        .pipe(gzip('Neon-' + manifest.version + '-firefox-development.zip'))
+        .pipe(gulp.dest('build'));
+});
 
 gulp.task('build:development:assets', ['build:clean'], (callback) => {
     Assets.build(Configuration).then(
