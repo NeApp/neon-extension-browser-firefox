9b32b0e48fc4398a0eef1df7e36effc63f47c7d3
Dean Gardiner <me@dgardiner.net>
2017-09-12 12:42:50 +1200

    Removed preferences module (moved to framework)

--- src/preferences/context.js
+++
@@ -1,5 +0,0 @@
-import {WebExtensionsPreferencesContext} from 'eon.extension.browser.base.webextensions/preferences/context';
-
-
-export class FirefoxPreferencesContext extends WebExtensionsPreferencesContext {
-}
