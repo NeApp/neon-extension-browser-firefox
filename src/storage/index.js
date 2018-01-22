5e2a4cebaf58edc512e86c5c06d422d85ffcb625
Dean Gardiner <me@dgardiner.net>
2018-01-22 15:33:39 +1300

    Removed unused `Storage.browser` property

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,13 +1,7 @@
-import FirefoxExtension from 'neon-extension-browser-firefox/extension';
 import {WebExtensionsStorage} from 'neon-extension-browser-webextension/storage';
 
 
 export class FirefoxStorage extends WebExtensionsStorage {
-    get browser() {
-        return {
-            extension: FirefoxExtension
-        };
-    }
 }
 
 export default new FirefoxStorage();
